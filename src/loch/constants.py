5c36578f27898609fa4bed667a46af01d0ace42b
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-03-02 23:47:07 +0200

    refactor(constants.py): added DEFAULT_SEMANTIC_EMBEDDING_MODEL_NAME

--- src/loch/constants.py
+++ src/loch/constants.py
@@ -5,3 +5,5 @@ LOCAL_PROJECT_PATH: Final[Path] = Path("./loch")
 LOCAL_DATABASES_PATH: Final[Path] = LOCAL_PROJECT_PATH / "databases"
 VECTOR_DB_NAME: Final[str] = "local-vector-db"
 VECTOR_DB_PATH: Final[Path] = LOCAL_DATABASES_PATH / VECTOR_DB_NAME
+
+DEFAULT_SEMANTIC_EMBEDDING_MODEL_NAME: Final[str] = "mixedbread-ai/mxbai-embed-large-v1"
