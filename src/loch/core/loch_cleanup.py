d51f73a0d3c1961b5addea69566a494e81905864
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-04-14 23:02:07 +0200

    feat(loch_cleanup.py): can delete a loch project

--- src/loch/core/loch_cleanup.py
+++ src/loch/core/loch_cleanup.py
@@ -2,6 +2,22 @@
 Core run when the user calls `loch cleanup`
 """
 
+import shutil
+
+from loch import constants
+
 def loch_cleanup():
     """Completely deletes the loch project in the current folder"""
-    print("ran loch cleanup")
+    if not constants.LOCAL_PROJECT_PATH.exists():
+        print(f"No local project found at '{constants.LOCAL_PROJECT_PATH}'")
+        exit()
+        
+    user_confirmation = input(
+        "Are you sure that you want to completely the [loch] project in the current folder?\n" +
+        "(anything response other than 'yes' will abort): "
+        )
+    if user_confirmation == "yes":
+        shutil.rmtree(constants.LOCAL_PROJECT_PATH)
+        print("[loch] project deleted")
+    else:
+        print("...process cancelled by user")
