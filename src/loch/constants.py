5322c3431531dc881fadb9b7e814782f7f50da93
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-04-14 22:54:55 +0200

    feat(constants.py): added project constants

---
+++ src/loch/constants.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from typing import Final
+
+LOCAL_PROJECT_PATH: Final[Path] = Path("./.loch")
+LOCAL_DATABASES_PATH: Final[Path] = LOCAL_PROJECT_PATH / "databases"
+VECTOR_DB_NAME: Final[str] = "local-vector-db"
+VECTOR_DB_PATH: Final[Path] = LOCAL_DATABASES_PATH / VECTOR_DB_NAME
+
+DEFAULT_SEMANTIC_EMBEDDING_MODEL_NAME: Final[str] = "minishlab/potion-base-8M"
