8bf3188fa71d74229b1cb75f43ccbd3c59e5d05b
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-04-14 22:58:08 +0200

    feat(core/loch_init.py): creates a new project

--- src/loch/core/loch_init.py
+++ src/loch/core/loch_init.py
@@ -2,5 +2,15 @@
 Core logic of the code run when the user calls `loch init`
 """
 
+from loch import constants
+
 def loch_init():
-    print("ran loch init")
+    if constants.LOCAL_PROJECT_PATH.exists():
+        print(
+            f"Existing [loch] project found in directory '{constants.LOCAL_PROJECT_PATH}'"
+        )
+        print("Run `loch cleanup` to delete the existing project")
+        exit()
+
+    constants.LOCAL_PROJECT_PATH.mkdir()
+    constants.LOCAL_DATABASES_PATH.mkdir()
