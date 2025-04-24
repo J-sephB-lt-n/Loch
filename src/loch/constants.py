5c15e2b43e0fb59d70692f943b3ada7eca769e4a
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-04-24 22:44:46 +0200

    feat(embeddings/model2vec.py-constants.py-uv.lock-pyproject.toml): caching of model2vec model in project cache works!

--- src/loch/constants.py
+++ src/loch/constants.py
@@ -7,11 +7,13 @@ ALG_NAMES: Final[BiMap] = BiMap(
     # TODO: should use the registry pattern here
     # refer also to algs/__init__.py
     [
+        ("code_review", "Code Review"),
         ("entire_doc_vector_search", "Entire Document Vector Search (semantic & BM25)"),
         ("llm_auto_tagging", "LLM-Generated Auto-Tagging"),
         ("llm_knowledge_graph", "LLM-Generated Knowledge Graph"),
         ("llm_question_answering", "LLM Content Question-Answering"),
         ("pyramid_search", "Agentic Knowledge Distillation (Pyramid Search)"),
+        ("semantic_code_search", "Semantic Code Search"),
     ],
 )
 
--- src/loch/llm/embeddings/model2vec.py
+++ src/loch/llm/embeddings/model2vec.py
@@ -5,15 +5,24 @@ Functions for working with the `https://github.com/MinishLab/model2vec` embeddin
 import logging
 from pathlib import Path
 
+import model2vec
+
 from loch import constants
 from loch.utils.logging_utils import get_logger
 
 logger: logging.Logger = get_logger(__name__)
 
-model2vec_download_dir: Path = constants.LOCAL_EMBEDDING_MODELS_PATH / "model2vec"
+MODEL2VEC_DOWNLOAD_DIR: Path = constants.LOCAL_EMBEDDING_MODELS_PATH / "model2vec"
 
-if not model2vec_download_dir.exists():
+if not MODEL2VEC_DOWNLOAD_DIR.exists():
     logger.info(
-        f"Downloading model2vec model `{constants.DEFAULT_MODEL2VEC_MODEL}` to `{model2vec_download_dir}`"
+        f"Downloading model2vec model `{constants.DEFAULT_MODEL2VEC_MODEL}` to `{MODEL2VEC_DOWNLOAD_DIR}`"
     )
-    model2vec_download_dir.mkdir()
+    MODEL2VEC_DOWNLOAD_DIR.mkdir()
+    embed_model = model2vec.StaticModel.from_pretrained(constants.DEFAULT_MODEL2VEC_MODEL)
+    embed_model.save_pretrained(MODEL2VEC_DOWNLOAD_DIR)
+else:
+    logger.info(
+        f"Loading previously downloaded model2vec model `{constants.DEFAULT_MODEL2VEC_MODEL}` from `{MODEL2VEC_DOWNLOAD_DIR}`"
+    )
+    embed_model = model2vec.StaticModel.from_pretrained(MODEL2VEC_DOWNLOAD_DIR)
