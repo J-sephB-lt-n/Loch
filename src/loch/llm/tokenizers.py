a774b296b4b15d46c1eb751b58c508eee534dc5f
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2025-04-29 23:48:59 +0200

    feat(llm/tokenizers.py): added conservative approach for unknown models

--- src/loch/llm/tokenizers.py
+++ src/loch/llm/tokenizers.py
@@ -2,14 +2,23 @@
 Functions related to tokenisation
 """
 
+import logging
+
+from loch.utils.logging_utils import get_logger
+
+logger: logging.Logger = get_logger(__name__)
+
 
 def count_approx_tokens(model_name: str, text: str) -> int:
     """
     Count approximate number of tokens in `text` for the given model
     """
     if model_name[:6] == "gemma3":
+        logger.info("Model is 'gemma3' - assuming 4 characters per token")
         return int(len(text) / 4)
     else:
-        raise ValueError(
-            f"Counting tokens for model '{model_name}' not currently supported"
+        logger.warning(
+            f"No token counting approach available for model '{model_name}'"
+            + " - assuming 3 characters per token"
         )
+        return int(len(text) / 3)
